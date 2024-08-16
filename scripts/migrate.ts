42f89f83f4fb07c948e59a2cd4d66cc3e1cc5564
Utsav Rawat <utsav.rawat@sjsu.edu>
2024-08-15 19:47:22 -0700

    add account module

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -3,8 +3,8 @@ import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
 import { migrate } from "drizzle-orm/neon-http/migrator";
 
-config({ path: ".env.local" });
-
+config({ path: ".env" });
+console.log("Database URL:", process.env.DATABASE_URL);
 const sql = neon(process.env.DATABASE_URL!);
 const db = drizzle(sql);
 
