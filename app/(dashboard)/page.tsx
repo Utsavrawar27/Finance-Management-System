a5909727319fffb0ebe1100e6c1e2c87266b485e
Utsav Rawat <utsav.rawat@sjsu.edu>
2024-08-18 14:39:11 -0700

    overview api

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { DataGrid } from "@/components/data-grid";
+import { DataCharts } from "@/components/data-charts";
 
-export default function Home() {
+export default function DashboardPage() {
   return (
-    <Button>
-      This is authenticated route
-    </Button>
-  )
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <DataGrid />
+      <DataCharts />
+    </div>
+  );
 }
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
+import summary from "./summary";
 import accounts from "./accounts";
 import categories from "./categories";
 import transactions from "./transactions";
@@ -10,11 +11,12 @@ export const runtime = "edge";
 const app = new Hono().basePath("/api");
 
 const routes = app
+  .route("/summary", summary)
   .route("/accounts", accounts)
   .route("/categories", categories)
   .route("/transactions", transactions);
-  
 
+  
 export const GET = handle(app);
 export const POST = handle(app);
 export const PATCH = handle(app);
